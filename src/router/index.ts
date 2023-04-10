c16d56b47821def8a0b75d51bd0b906babefcb9c
zengxingqi <me@zengxingqi.com>
2023-04-10 18:36:44 +0800

    feat: token

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, NavigationFailureType, isNavigationFailure } from 'vue-router'
+import type { Router } from 'vue-router'
 import { base } from '@/router/routes'
 import {
   beforeEach,
   afterEach
 } from '@/router/interceptor'
-const router = createRouter({
+// import {
+//   BASE_PAGE_LOGIN
+// } from '@/router/routesName'
+const initRouter = (): Router => createRouter({
   history: createWebHistory(),
   routes: base,
   scrollBehavior (to, from, savedPosition) {
@@ -17,8 +21,25 @@ const router = createRouter({
   // 区分路由大小写，以及带有或不带有尾部斜线的路由
   strict: true
 })
+const router = initRouter()
+console.log('router', router)
 
 router.beforeEach(beforeEach)
 router.afterEach(afterEach)
+// 重置路由列表
+export function resetRouter (): void {
 
+}
+// 使用 push 等待导航结果
+export async function goToRouteName (name: string): Promise<void> {
+  const navigationResult = await router.push({ name })
+  // 给用户显示一个小通知
+  if (isNavigationFailure(navigationResult, NavigationFailureType.aborted)) {
+    console.warn('导航被中断了!')
+  } else if (isNavigationFailure(navigationResult, NavigationFailureType.cancelled)) {
+    console.warn('导航被取消了!')
+  } else if (isNavigationFailure(navigationResult, NavigationFailureType.duplicated)) {
+    console.warn('导航被阻止了!')
+  }
+}
 export default router
---
+++ src/store/token.ts
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia'
+import { useLocalStorage } from '@vueuse/core'
+// import type { RemovableRef } from '@vueuse/core'
+import {} from '@/router/index'
+import { TOKEN_CODE } from '@/config/constant/tokenStore'
+import type { UseTokenStoreTypes } from '@/config/typings/tokenStore'
+export const useTokenStore = defineStore('token', (): UseTokenStoreTypes => {
+  const token = useLocalStorage(TOKEN_CODE, '') // 接口请求令牌
+  return { token }
+})
