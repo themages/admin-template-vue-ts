7805c855eab9306b9cd0418d3999e4c3db8d03e4
zengxingqi <me@zengxingqi.com>
2023-04-12 18:20:32 +0800

    feat: http utils

--- src/store/user.ts
+++ src/store/user.ts
@@ -4,7 +4,7 @@ import type { UserInfoTypes, UseUserStoreTypes } from '@/config/typings/userStor
 import { UserAccountStatusEnum } from '@/enums/user/roles'
 export const useUserStore = defineStore('user', (): UseUserStoreTypes => {
   const userinfo = ref<UserInfoTypes | null>(null) // 用户信息
-  const accountStatus = ref<number>(UserAccountStatusEnum.registered) // 账号状态
+  const accountStatus = ref<number>(UserAccountStatusEnum.default) // 账号状态
   const codes = ref<string[]>([]) // 页面角色权限code
   return { userinfo, codes, accountStatus }
 })
